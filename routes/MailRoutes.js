9a49a98e7771d1e19db20be4e1aa3cc68aaf8f51
Daniel Obon <daniel.obon@creativecoefficient.net>
2019-10-28 17:14:24 -0300

    email validation (1st steps)

--- routes/MailRoutes.js
+++ routes/MailRoutes.js
@@ -5,6 +5,8 @@ const Validator = require("./utils/Validator");
 const Messages = require("../constants/Messages");
 const Constants = require("../constants/Constants");
 
+var mailgun = require("mailgun-js")({ apiKey: Constants.MAILGUN_API_KEY, domain: Constants.MAILGUN_DOMAIN });
+
 /*
 	Validación del email. El usuario debe envia su mail personal para poder
 	generar una validación a través del envio de un correo electronico. retorna result =
@@ -21,7 +23,21 @@ router.post(
 		const timeStamp = req.body.timeStamp;
 		const eMail = req.body.eMail;
 
-		return ResponseHandler.sendRes(res, "TODO");
+		const data = {
+			from: Messages.EMAIL.VALIDATION.FROM,
+			to: eMail,
+			subject: Messages.EMAIL.VALIDATION.SUBJECT,
+			text: Messages.EMAIL.VALIDATION.MESSAGE("123456")
+		};
+
+		mailgun.messages().send(data, (error, body) => {
+			if (error) {
+				console.log(error);
+				return ResponseHandler.sendErr(res, Messages.EMAIL.ERR.COMMUNICATION_ERROR);
+			}
+			return ResponseHandler.sendRes(res, "");
+		});
+
 		// "SUCCESS"
 		// "COMMUNICATION_ERROR"
 	}
