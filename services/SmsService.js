4919333df57f947e77e351786a372d46754dfc4e
Daniel Obon <daniel.obon@creativecoefficient.net>
2019-10-31 10:49:36 -0300

    sms working

--- services/SmsService.js
+++ services/SmsService.js
@@ -1,17 +1,25 @@
 const Phone = require("../models/Phone");
+
 const Messages = require("../constants/Messages");
+const Constants = require("../constants/Constants");
+
+const twilio = require("twilio");
 
 class SmsService {
 	static sendValidationCode(phoneNumber, code, cb, errCb) {
 		const data = {
-			from: Messages.SMS.VALIDATION.FROM,
-			to: phoneNumber,
-			subject: Messages.SMS.VALIDATION.SUBJECT,
-			text: Messages.SMS.VALIDATION.MESSAGE(code)
+			body: Messages.SMS.VALIDATION.MESSAGE(code),
+			to: Constants.PHONE_REGION + phoneNumber.substring(phoneNumber.length - 8),
+			from: Constants.TWILIO_PHONE_NUMBER
 		};
 
-		// TODO send sms
-		return cb();
+		var client = twilio(Constants.TWILIO_SID, Constants.TWILIO_TOKEN);
+		if (Constants.DEBUGG) console.log(Messages.SMS.SENDING(data.to));
+		client.messages.create(data, function(err, message) {
+			if (err) return errCb(err);
+			if (Constants.DEBUGG) console.log(Messages.SMS.SENT);
+			return cb(message);
+		});
 	}
 
 	static create(phoneNumber, code, did, cb, errCb) {
