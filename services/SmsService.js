b30165ca1a536add264efa71d7efec62406188ef
Daniel Obon <daniel.obon@creativecoefficient.net>
2019-11-06 11:35:14 -0300

    try-catch twillio & mailgun

--- services/SmsService.js
+++ services/SmsService.js
@@ -15,10 +15,12 @@ module.exports.sendValidationCode = async function(phoneNumber, code) {
 	var client = twilio(Constants.TWILIO_SID, Constants.TWILIO_TOKEN);
 	if (Constants.DEBUGG) console.log(Messages.SMS.SENDING(data.to));
 
-	client.messages.create(data, function(err, _) {
-		if (err) return Promise.reject(err);
+	try {
+		await client.messages.create(data);
 		if (Constants.DEBUGG) console.log(Messages.SMS.SENT);
-	});
+	} catch(err) {
+		console.log(err);
+	}
 };
 
 module.exports.create = async function(phoneNumber, code, did) {
