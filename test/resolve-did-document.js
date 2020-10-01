9230250a897cc5ce89b8054bed76695a37a04f05
fsawadyatix <fsawady@atixlabs.com>
2020-10-01 15:20:09 -0300

    feat: better scripts

--- test/resolve-did-document.js
+++ test/resolve-did-document.js
@@ -4,16 +4,19 @@ const { getResolver } = require("ethr-did-resolver");
 // const BLOCK_CHAIN_URL = "http://testnet.rsk.didi.org.ar:4444"; // RSK
 // const BLOCK_CHAIN_URL = "http://45.79.211.34:4444";
 // const BLOCK_CHAIN_URL = "https://rinkeby.infura.io/v3/5dcd1d1dbdef433f88f8a23dc862f656"; // ETH
+// const BLOCK_CHAIN_URL = "http://45.79.252.246:4545" // Lacchain
+// const BLOCK_CHAIN_URL = "http://writer.lacchain.net:4545"; // Lacchain
 
-const BLOCK_CHAIN_CONTRACT = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b";
+// const BLOCK_CHAIN_CONTRACT = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b";
+// const BLOCK_CHAIN_CONTRACT = "0x488C83c4D1dDCF8f3696273eCcf0Ff4Cf54Bf277" // Lacchain
 
 const ISSUER_SERVER_DID = "did:ethr:0x31a6beeda81f05bdf24e14a34bda7fa3d9e4f1b9";
 
 const resolver = new Resolver(getResolver({ rpcUrl: BLOCK_CHAIN_URL, registry: BLOCK_CHAIN_CONTRACT }));
 
 async function logResolve() {
-    const resolv = await resolver.resolve(ISSUER_SERVER_DID);
-    console.log("DID resuelto: ", JSON.stringify(resolv));
+	const resolv = await resolver.resolve(ISSUER_SERVER_DID);
+	console.log("DID resuelto: ", JSON.stringify(resolv));
 }
 
 logResolve();
