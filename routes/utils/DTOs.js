e72ea3b018eb3857d09429f749400b9115d6e52a
Nicolas Laitan <nlaitan@atixlabs.com>
2021-02-09 14:07:04 -0300

    fix: add doc to dto function

--- routes/utils/DTOs.js
+++ routes/utils/DTOs.js
@@ -1,5 +1,7 @@
 const { getImageUrl } = require("./Helpers");
 
+// DTO: Data Transfer Object
+// Selecciona las propiedades del usuario que se pueden exponer y las retorna
 const userDTO = async (user, extra = {}) => {
 	const mail = await user.getMail();
 	const phoneNumber = await user.getPhoneNumber();
