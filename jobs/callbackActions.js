37cecae9d5872bd91be34f9f35bf688e1bda6312
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2021-03-22 17:12:47 -0300

    Ejecutar callback en jobs solo si existe url MM-150 #time 1h 30m

--- jobs/callbackActions.js
+++ jobs/callbackActions.js
@@ -1,8 +1,13 @@
 const IssuerService = require("../services/IssuerService");
 
 const sendCallbackIssuer = async callbackTask => {
-	const { _id, callbackUrl, did, token, status, expireOn, blockHash, messageError } = callbackTask;
-	await IssuerService.callback(callbackUrl, did, token, { status, expireOn, blockHash, messageError });
+	const { 
+		_id, callbackUrl, did, token, status, expireOn, blockHash, messageError,
+	} = callbackTask;
+	if (!callbackUrl) return;
+	await IssuerService.callback(callbackUrl, did, token, { 
+		status, expireOn, blockHash, messageError 
+	});
 };
 
 exports.actions = {
