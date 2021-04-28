3f6240fc1033ef3a8de286271047e521b2b78edc
Diegoro21 <diero17@gmail.com>
2021-04-28 12:58:39 -0300

    Documentacion rutas AdminRoutes MM-199

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -13,9 +13,26 @@ const Validator = require("./utils/Validator");
 router.use("/admin/", validateAdminJWT);
 
 /**
- *	Obtiene informacion confidencial sobre el usuario según su did
- *  (Incluye status de renaper y de semillas)
+ * @openapi
+ *   /admin/user/did/:{did}:
+ *   get:
+ *     summary: Obtiene información confidencial sobre el usuario según su did.
+ *     description: Incluye status de renaper y de semillas.
+ *     parameters:
+ *       - name: did
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/admin/user/did/:did",
 	Validator.checkValidationResult,
@@ -44,8 +61,29 @@ router.get(
 );
 
 /**
- *	Obtiene informacion confidencial sobre el usuario según su numero de teléfono
+ * @openapi
+ *   /admin/user/phone:
+ *   post:
+ *     summary: Obtiene informacion confidencial sobre el usuario según su numero de teléfono.
+ *     requestBody:
+ *       required:
+ *         - phone
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.post("/admin/user/phone", Validator.checkValidationResult, Validator.validateParams, async function (req, res) {
 	try {
 		const { phone } = req.body;
