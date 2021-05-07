c55431ab1c1c72dc8c7603e011c67a02454fdee5
Diegoro21 <diero17@gmail.com>
2021-05-07 12:06:43 -0300

    Asegurar calidad de services/AuthRequestService.js MM-317

--- constants/serviceErrors.js
+++ constants/serviceErrors.js
@@ -7,4 +7,20 @@ module.exports = {
     code: '#service-missingName',
     message: 'Falta el parámetro NAME.',
   },
+  missingOperationId: {
+    code: '#service-missingOperationId',
+    message: 'Falta el parámetro OPERATIONID.',
+  },
+  missingUserDID: {
+    code: '#service-missingUserDID',
+    message: 'Falta el parámetro USERDID.',
+  },
+  missingErrMsg: {
+    code: '#service-missingErrMsg',
+    message: 'Falta el parámetro ERRMSG.',
+  },
+  missingStatus: {
+    code: '#service-missingStatus',
+    message: 'Falta el parámetro STATUS.',
+  },
 };
