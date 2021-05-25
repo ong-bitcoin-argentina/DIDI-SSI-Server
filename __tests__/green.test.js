c19de1f7debbec262420fdd7c95802d70d4a2cf7
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-05-25 13:30:20 -0300

    Add mongoose connection to testing suite

--- __tests__/green.test.js
+++ __tests__/green.test.js
@@ -1,4 +1,19 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const {
+  MONGO_URL,
+} = require('../constants/Constants');
+
 describe('Should be green', () => {
+  beforeAll(async () => {
+    await mongoose
+      .connect(MONGO_URL, {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      });
+  }, 60000);
   test('Expect true to be true', () => {
     expect(true).toBe(true);
   });
