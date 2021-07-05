51e68433efd8a6576820f82a30b0dcd955f1ac32
fedelmar <fsuarez91@gmail.com>
2021-07-05 11:28:03 -0300

    update firebase test -

--- __tests__/services/Firebase.test.js
+++ __tests__/services/Firebase.test.js
@@ -1,30 +1,14 @@
 const { sendPushNotification } = require('../../services/FirebaseService');
 const {
-  missingType, missingMessage, missingFirebaseId,
+  missingFirebaseId,
 } = require('../../constants/serviceErrors');
 
 describe('Should be green', () => {
-  test('Expect sendPushNotification to throw on missing message', async () => {
-    try {
-      await sendPushNotification('title', undefined, 'firebaseId', 'type');
-    } catch (e) {
-      expect(e.code).toMatch(missingMessage.code);
-    }
-  });
-
   test('Expect sendPushNotification to throw on missing firebaseId', async () => {
     try {
       await sendPushNotification('title', 'message', undefined, 'type');
     } catch (e) {
       expect(e.code).toMatch(missingFirebaseId.code);
-    }
-  });
-
-  test('Expect sendPushNotification to throw on missing type', async () => {
-    try {
-      await sendPushNotification('title', 'message', 'firebaseId', undefined);
-    } catch (e) {
-      expect(e.code).toMatch(missingType.code);
     }
   });
 });
