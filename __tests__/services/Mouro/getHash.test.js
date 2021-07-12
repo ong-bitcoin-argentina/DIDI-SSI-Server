9556484a3e95f6da1e6401107b1745d3bad092ba
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-07-12 19:50:17 -0300

    Fix linter

--- __tests__/services/Mouro/getHash.test.js
+++ __tests__/services/Mouro/getHash.test.js
@@ -1,6 +1,5 @@
 const { getHash } = require('../../../services/MouroService');
 const { missingDid } = require('../../../constants/serviceErrors');
-const { appData } = require('./constants');
 
 describe('services/Mouro/getHash.test.js', () => {
   test('Expect getHash to throw on missing did', async () => {
